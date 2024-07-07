f8f7e8d7822b013fa9c6f0ac540c875850025f68
Mehrdad <52593172+mehrdad987@users.noreply.github.com>
2024-07-07 10:58:06 +0200

    Update msd.ino

--- sd/msd.ino
+++ sd/msd.ino
@@ -1,46 +1,50 @@
 /*
   SD card read/write
- 
- This example shows how to read and write data to and from an SD card file   
- The circuit:
 
  */
  
 #include <SPI.h>
-#include <mySD.h>
+#include <SD.h>
 
-File myFile;
+File file;
 
-const int chipSelect = 2;
+// change this to match your SD shield or module;
+//     Arduino Ethernet shield: pin 4
+//     Adafruit SD shields and modules: pin 10
+//     Sparkfun SD shield: pin 8
+const int CS = 2;
 
 void setup()
 {
  // Open serial communications and wait for port to open:
   Serial.begin(115200);
    while (!Serial) {
-    ; 
+    ; // wait for serial port to connect. Needed for Leonardo only
   }
 
 
   Serial.print("Initializing SD card...");
-
-   pinMode(SS, OUTPUT);
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work. 
    
-  if (!SD.begin(2, 23, 19, 18)) {
+  if (!SD.begin(2)) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
   
   // open the file. note that only one file can be open at a time,
-  myFile = SD.open("test.csv", FILE_WRITE);
+  // so you have to close this one before opening another.
+  file = SD.open("test.csv", FILE_WRITE);
   
   // if the file opened okay, write to it:
-  if (myFile) {
+  if (file) {
     Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
+    file.println("testing 1, 2, 3.");
   // close the file:
-    myFile.close();
+    file.close();
     Serial.println("done.");
   } else {
     // if the file didn't open, print an error:
@@ -48,16 +52,16 @@ void setup()
   }
   
   // re-open the file for reading:
-  myFile = SD.open("test.csv");
-  if (myFile) {
+  file = SD.open("test.csv");
+  if (file) {
     Serial.println("test.csv:");
     
     // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
+    while (file.available()) {
+      Serial.write(file.read());
     }
     // close the file:
-    myFile.close();
+    file.close();
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
@@ -66,4 +70,5 @@ void setup()
 
 void loop()
 {
+  // nothing happens after setup
 }
